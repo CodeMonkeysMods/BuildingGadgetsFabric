1443ae6b316f842c7a3adf3c5dbf18d50ffd073a
The-Code-Monkey <31218527+The-Code-Monkey@users.noreply.github.com>
2020-08-12 22:59:51 +0100

    Update and rename src/main/java/net/fabricmc/example/ExampleMod.java to src/main/java/net/the-code-monkey/BuildingGadgets.java

---
+++ src/main/java/net/the-code-monkey/BuildingGadgets.java
@@ -0,0 +1,10 @@
+package net.the-code-monkey;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BuildingGadgets implements ModInitializer {
+	public final String MODID = "BuildingGadgets";
+
+        @Override
+	public void onInitialize() {}
+}
